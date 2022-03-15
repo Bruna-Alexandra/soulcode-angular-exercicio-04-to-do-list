8d6d306f4274887fc97a073eb04458ffde088e81
Bruna-Alexandra <mittyc4@gmail.com>
2022-03-15 18:16:59 -0300

    mudanças finais

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output, NgModule, ViewChild, ElementRef} from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, NgModule, ViewChild, ElementRef } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -21,44 +21,37 @@ export class ListComponent implements OnInit {
   constructor(private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-    if (this.count != "1"){
-      localStorage.setItem('list', JSON.stringify(this.items))
-      localStorage.setItem('count', '1')
-    }
-    else{
-      this.items = this.getItems
-    }
-
+    this.items = this.getItems
   }
   addItem(item: string): void | string {
-    if (this.items.length == 10){
+    if (this.items.length == 10) {
       this.openSnackBar('Your list is full, delete an item first.', 'Ok')
     }
 
-    else if (item.length > 20) {
-      this.openSnackBar("Maximum of 20 characters.", 'Ok');
+    else if (item.length > 18) {
+      this.openSnackBar("Maximum of 18 characters.", 'Ok');
 
     }
     else if (item.length < 4) {
       this.openSnackBar("Minimum of 4 characters.", 'Ok');
     }
     else {
-/*       this.items.unshift(item); */
       this.ipt.nativeElement.value = ""
-        this.items = this.getItems
-        this.items.unshift(item)
-        localStorage.setItem('list', JSON.stringify(this.items))
-    }}
-  
+      this.items = this.getItems
+      this.items.unshift(item)
+      localStorage.setItem('list', JSON.stringify(this.items))
+    }
+  }
+
 
   removeItem(index: number): void {
     this.items.splice(index, 1)
     localStorage.setItem('list', JSON.stringify(this.items))
   }
 
-  clearAll(deletions: number): void{
-    
-    this.items.splice(0, deletions-5)
+  clearAll(deletions: number): void {
+
+    this.items.splice(0, deletions - 5)
     localStorage.setItem('list', JSON.stringify(this.items))
   }
 
